3270ad9c410cbb5761b2f56fb6c39f523dc5afc3
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2024-03-06 23:30:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let input = prompt("Enter a input ?");
+
+let number = parseInt(input);
+let fact = 1;
+for(let i=1; i<=number; i++)
+	{
+		fact = fact*i;
+	}
+alert("The factorial of "+number+" is "+fact);
